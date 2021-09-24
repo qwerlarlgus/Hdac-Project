b0a826aa57535eff3ec101a3e046a1c193079022
Anthony KIM <75463182+qwerlarlgus@users.noreply.github.com>
2021-09-24 17:18:03 +0900

    back upload

--- back/routes/route.js
+++ back/routes/route.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const router = express.Router();
-const controller = require('../controllers/controller')
+const router = require("express").Router();
+const { api } = require("../api");
+const controller = require("../controllers/controller");
 
-router.get('/oauth/kakao', controller.getAuthenticate);
-router.get('/oauth/kakao/callback', controller.authCallBack);
-router.get('/oauth/kakao/logout', controller.logout);
+router.get("/", controller.welcome);
 
-router.get('/success', controller.success);
-router.get('/fail', controller.fail);
+router.get("/oauth/kakao", controller.getAuthenticate);
+router.get("/oauth/kakao/callback", controller.authCallBack);
+router.get("/oauth/kakao/logout", controller.logout);
 
-router.get('/f1', controller.f1);
-router.get('/f2', controller.f2);
+router.get("/success", controller.success);
+router.get("/fail", controller.fail);
 
+// router.get("/whoami", controller.whoami);
 
-router.get('/api/:alias', controller.getQuery);
-router.post('/api/:alias', controller.postQuery);
+router.use("/api", api);
 
+// router.get("/api/:alias", controller.getQuery);
+// router.post("/api/:alias", controller.postQuery);
 
 module.exports = router;
